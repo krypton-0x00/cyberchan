29188c361d4f441e31f9b7794cf91fbfd319a450
krypton-0x00 <shakirgulzar0x00@gmail.com>
2024-05-23 14:16:57 +0100

    bug fixx

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -43,14 +43,11 @@ export default function SignUp() {
         {/* left */}
         <div className="flex-1">
           <Link to="/" className="font-bold dark:text-white text-4xl">
-            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-              Sahand's
-            </span>
-            Blog
+            <span className="px-2 py-1   text-white">Cyber</span>
+            Chan
           </Link>
           <p className="text-sm mt-5">
-            This is a demo project. You can sign up with your email and password
-            or with Google.
+            You can sign up with your email and password or with Google.
           </p>
         </div>
         {/* right */}
